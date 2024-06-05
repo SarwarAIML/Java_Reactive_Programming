a4b13558858337a89a9b0a0c28ae272460910d86
Gulam Sarwar <gsarwar@altimetrik.com>
2024-06-05 10:55:07 +0530

    Adavnced dnn,Conversational AI, mlops,NLP application

---
+++ webflux-demo/src/test/java/com/gs/webflux/WebfluxDemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.gs.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.nio.file.WatchEvent;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class BaseTest {
+
+}
